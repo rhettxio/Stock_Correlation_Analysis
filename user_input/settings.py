de503cd869bf767a3518e65b6dce35282651d178
anqi <xaq24117@gmail.com>
2020-06-23 05:03:38 -0400

    Create settings.py

---
+++ user_input/settings.py
@@ -0,0 +1,8 @@
+#global parameters
+
+LIST_STOCKS = ["\"AAPL\"","\"AMZN\"","\"MSFT\"","\"JPM\"","\"GS\"","\"PFE\""]
+
+START_DATE = "2015-06-01"
+END_DATE = "2020-06-01"
+
+params = [LIST_STOCKS, START_DATE, END_DATE]
