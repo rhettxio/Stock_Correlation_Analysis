82f0351e5f86366498ec1bf2c02e2e43472f06a4
anqi <xaq24117@gmail.com>
2020-06-24 18:51:51 -0400

    Update settings.py

--- user_input/settings.py
+++ user_input/settings.py
@@ -1,6 +1,6 @@
 #global parameters
 
-LIST_STOCKS = ["\"AAPL\"","\"AMZN\"","\"MSFT\"","\"JPM\"","\"GS\"","\"PFE\""]
+LIST_STOCKS = ["AAPL","AMZN","MSFT","JPM","GS","PFE"]
 
 START_DATE = "2015-06-01"
 END_DATE = "2020-06-01"
